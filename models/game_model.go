0b71f1cbec2049ed1860f5537d647db177ca15d3
jugendhacker <j.r@jugendhacker.de>
2020-09-05 20:34:51 +0200

    Make camel case consistent

--- models/game_model.go
+++ models/game_model.go
@@ -9,7 +9,7 @@ import (
 
 type Game struct {
 	ID           string `gorm:"uniqueIndex" sql:"primary_key;defautl:uuid_generate_v4()" json:"id"`
-	GamePin      uint64 `json:"game-pin"`
+	GamePin      uint64 `json:"gamePin"`
 	CreatorName  string `json:"creatorName"`
 	TeamMateName string `json:"teammateName"`
 }
--- router/router.go
+++ router/router.go
@@ -4,8 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func CreateRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/join")
 	router.POST("/create", CreateGameRoute)
+	router.POST("/join/:gamePin")
 
 	return router
 }
